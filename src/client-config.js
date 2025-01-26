49c9cf2a79c4c12816f436e7451c2e6aadec3389
R <aparnr@deloitte.com>
2025-01-26 12:47:01 +0530

    add the files

--- src/client-config.js
+++ src/client-config.js
@@ -17,15 +17,15 @@ window.config = {
   MINIO_URL: 'http://localhost:3535/ocrvs/',
   COUNTRY_CONFIG_URL: 'http://localhost:3040',
   // Country code in uppercase ALPHA-3 format
-  COUNTRY: 'FAR',
-  LANGUAGES: 'en,fr',
+  COUNTRY: 'IND',
+  LANGUAGES: 'en,fr,ta',
   SENTRY: '',
   // Use the values in comments when Metabase is running locally
   // http://localhost:4444/public/dashboard/acae0527-74be-4804-a3ee-f8b3c9c8784c#bordered=false&titled=false&refresh=300
-  LEADERBOARDS_DASHBOARD_URL: '',
+  LEADERBOARDS_DASHBOARD_URL: 'http://localhost:4444/public/dashboard/acae0527-74be-4804-a3ee-f8b3c9c8784c#bordered=false&titled=false&refresh=300',
   // http://localhost:4444/public/dashboard/fec78656-e4f9-4b51-b540-0fed81dbd821#bordered=false&titled=false&refresh=300
-  REGISTRATIONS_DASHBOARD_URL: '',
+  REGISTRATIONS_DASHBOARD_URL: 'http://localhost:4444/public/dashboard/fec78656-e4f9-4b51-b540-0fed81dbd821#bordered=false&titled=false&refresh=300',
   // http://localhost:4444/public/dashboard/a17e9bc0-15a2-4bd1-92fa-ab0f346227ca#bordered=false&titled=false&refresh=300
-  STATISTICS_DASHBOARD_URL: '',
+  STATISTICS_DASHBOARD_URL: 'http://localhost:4444/public/dashboard/a17e9bc0-15a2-4bd1-92fa-ab0f346227ca#bordered=false&titled=false&refresh=300',
   FEATURES: {}
 }
